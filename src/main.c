9f0a7897f8cca574ac1ab27cf5af59d1684a900a
Prudhvi Krishna Surapaneni <p@fbsd.in>
2010-11-30 10:46:13 -0600

    Handle SIGPIPE

--- src/main.c
+++ src/main.c
@@ -1,11 +1,18 @@
+#include <signal.h>
 #include <stdlib.h>
 
 #include "server.h"
 
 int main(int argc,char * argv[]) {
 	server_t * gop_handle;
+	struct sigaction act;
 
 	gop_handle = gop_init(argc,argv);
+	
+	act.sa_handler = SIG_IGN;
+	sigemptyset(&act.sa_mask);
+	act.sa_flags = 0;
+	sigaction(SIGPIPE,&act,NULL);
 
 	if(gop_handle) {
 		gop_serve_forever(gop_handle);
